d6c4c9896db37e5bdccf971490c20ce4b2b2c1cd
BertVoorjans <bert.voorjans@student.ehb.be>
2023-09-11 00:51:52 +0200

    cleanup testcode add class to every 16th square.

--- scripts/script.js
+++ scripts/script.js
@@ -6,9 +6,5 @@ const container = document.querySelector("#container");
 for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.classList.add("grid");
-    if ((i + 1) % 16 == 0) {
-        div.classList.add("x16");
-    }
     container.appendChild(div);
-    console.log("divs added:" + (i + 1));
 };
