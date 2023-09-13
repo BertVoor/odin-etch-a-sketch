67865fe8b70ad8f045bb8b8ce35b8f6f9c32077a
Bert <voorjansbert@gmail.com>
2023-09-13 22:09:54 +0200

    added functionality to reset button

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,24 @@
 console.log("script loaded");
 
-const container = document.querySelector("#playGrid");
+const playGrid = document.querySelector("#playGrid");
 
 for (let i = 0; i < 256; i++) {
 	const div = document.createElement("div");
 	div.classList.add("grid");
-	container.appendChild(div);
+	playGrid.appendChild(div);
 }
 
 const pixels = document.querySelectorAll(".grid");
 pixels.forEach((pixel) => {
 	pixel.addEventListener("mouseover", (e) => {
 		e.target.classList.add("coloured");
+	});
+});
+
+const resetButton = document.querySelector("#resetButton");
+
+resetButton.addEventListener("click", () => {
+	pixels.forEach((pixel) => {
+		pixel.classList.remove("coloured");
 	});
 });
