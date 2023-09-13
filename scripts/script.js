a96fa609b98e0d01c73678295489a29e069fcdc0
Bert <voorjansbert@gmail.com>
2023-09-13 21:20:16 +0200

    added reset button, changed name of #container to #playGrid.

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,16 @@
 console.log("script loaded");
 
-
-const container = document.querySelector("#container");
+const container = document.querySelector("#playGrid");
 
 for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    div.classList.add("grid");
-    container.appendChild(div);
-};
+	const div = document.createElement("div");
+	div.classList.add("grid");
+	container.appendChild(div);
+}
 
 const pixels = document.querySelectorAll(".grid");
 pixels.forEach((pixel) => {
-    pixel.addEventListener("mouseover", (e) => {
-        e.target.classList.add("coloured");
-    })
+	pixel.addEventListener("mouseover", (e) => {
+		e.target.classList.add("coloured");
+	});
 });
